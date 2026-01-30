d4b57808b7336a967fb5f837746184127f6f68b9
Ru3enG <rubengilcastello@gmail.com>
2026-01-30 17:29:59 +0100

    Viernes30

--- proyecto/src/main/java/com/proyecto/proyecto/dto/UsuarioDTO.java
+++ proyecto/src/main/java/com/proyecto/proyecto/dto/UsuarioDTO.java
@@ -2,24 +2,18 @@ package com.proyecto.proyecto.dto;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class UsuarioDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String username;
     private String password;
